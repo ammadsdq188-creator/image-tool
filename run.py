5cf132767fc924f81bd293e79285bdbd28f510c0
Ammad <ammadsdq@gmail.com>
2026-08-12 17:18:18 +0500

    Containerize image resizer

--- run.py
+++ run.py
@@ -2,4 +2,8 @@ from app import app
 from app import routes
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=False
+    )
